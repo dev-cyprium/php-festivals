9f12a61fe7599e38aaf255c699c0ed9b44a0018d
Stefan Kupresak <stefan_vg@hotmail.com>
2018-05-23 19:42:54 +0200

    Add items in the header

--- views/header.php
+++ views/header.php
@@ -1,2 +1,10 @@
 <header>
+    <nav>
+        <ul>
+            <li><a href='#'>Početna</a></li>
+            <li><a href='#'>Festivali</a></li>
+            <li><a href='#'>Autor</a></li>
+            <li><a href='#'>Kontakt</a></li>
+        </ul>
+    </nav>
 </header>
