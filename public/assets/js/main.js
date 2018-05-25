f7e5f7a2df2c8e673267f368dea48d49d521996d
Stefan Kupresak <stefan_vg@hotmail.com>
2018-05-25 03:52:15 +0200

    Add landing page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,26 @@
 $(document).ready(() => {
-    alert('loaded');
+    animateMap();
 });
+
+function Markers() {
+    this.markers = $(".circle-star");
+    this.position = 0;
+}
+
+Markers.prototype.next = function() {
+    return $(this.markers[this.position++]);
+}
+
+
+function animateMap() {
+    var markers = new Markers();
+    animateMarker(markers.next());
+}
+
+function animateMarker(marker) {
+    if(!marker) return;
+
+    marker.addClass('active');
+
+    setTimeout(() => animateMarker(marker.next()), 100);
+}
--- views/pocetna.php
+++ views/pocetna.php
@@ -1,1 +1,19 @@
-<?= file_get_contents(PROJECT_ROOT . "/public/assets/images/serbia.svg"); ?>
+<div id="center">
+    <div id="hero-title">
+        <h1>
+            Festivian
+        </h1>
+        <p>
+            Pronađite bilo koji festival u Srbiji.
+            Brzo, lako i jednostavno!
+        </p>
+        <p>
+            Planirajte unapred i rezervišite karte 
+            već danas!
+        </p>
+        <a href='#' class='btn-pretraizi'>Počni pertragu</a>
+    </div>
+    <div id='map'>
+        <?= file_get_contents(PROJECT_ROOT . "/public/assets/images/serbia.svg"); ?>
+    </div>
+</div>
