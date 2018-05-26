b518fad92d1e2a0b781190c4ebea9ca99a3d9f4f
Stefan Kupresak <stefan_vg@hotmail.com>
2018-05-26 04:53:29 +0200

    Add more styles to the website

--- views/pocetna.php
+++ views/pocetna.php
@@ -17,3 +17,15 @@
         <?= file_get_contents(PROJECT_ROOT . "/public/assets/images/serbia.svg"); ?>
     </div>
 </div>
+<div class='bg-wrap'>
+    <div id="acc-tabs">
+        <div class='tab active'>
+            <i class="fas fa-user"></i>
+        </div>
+        <div class='tab'>
+            <i class="fas fa-users"></i>
+        </div>
+    </div>
+    <div id="tab-content">
+    </div>
+</div>
