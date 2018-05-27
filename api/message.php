6aeba959b1bb5661733db2134e0a10163a5c8947
Stefan Kupresak <stefan_vg@hotmail.com>
2018-05-27 11:17:30 +0200

    add success example

--- api/message.php
+++ api/message.php
@@ -1,2 +1,9 @@
 <?php
-    echo "Hi!";
+    $message = $_POST['message'];
+    $email   = $_POST['email'];
+
+    $msgReg = '/.{10,50}/';
+    
+    echo json_encode([
+        "success" => true
+    ]);
