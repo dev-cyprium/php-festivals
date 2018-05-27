a66acc74a4362ffc28e695ed6ca07f5e596d963e
Stefan Kupresak <stefan_vg@hotmail.com>
2018-05-27 02:27:32 +0200

    Finish the prototype alert system

--- assets/js/alerts.js
+++ assets/js/alerts.js
@@ -1,6 +1,26 @@
+import $ from 'jquery'
+
+function template(message) {
+    return `
+      <div class='alert'>
+        <span class='message'>${message}</span>
+        <span class='closebtn' onclick="this.parentElement.style.display='none';">
+        &times;</span>
+      </div>
+    `;
+}
+
+let el = null;
+
 const Alerts = {
     createAlert: (message) => {
-        
+        if(el) {
+            el.css("display", "block");
+            el.find('.message').text(message);
+        } else {
+            el = $(template(message))
+            $('body').append(el)
+        }
     }
 }
 
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
 // Main JS file
 import Map from "./map.js";
 import Tabs from "./tabs.js";
+import alerts from "./alerts";
+
+window.App = {}
+App.alerts = alerts
 
 function boot() {
     let map = new Map();
