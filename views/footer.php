6b219b5509b71a920e71deee2cd928163e8a2f44
Stefan Kupresak <stefan_vg@hotmail.com>
2018-06-05 14:22:51 +0200

    Finalize the footer design

--- views/footer.php
+++ views/footer.php
@@ -1,3 +1,30 @@
 <footer>
-    <script src="/assets/js/app.js"></script>
+    <div class='footer-wrap'>
+        <div class='footer-center'>
+            <div class='col'>
+                <h1>O Festivian</h1>
+                <p>
+                    Festivian platforma je moderna, skalabilna
+                    platforma koja omogucava brz, lak, i ekonomičan, jednostavan
+                    pristup festivalima i korisnicima i organizatorima
+                </p>
+            </div>
+            <div class='col-2'>
+                <h1>Konektujte se</h1>
+                <div class='icons-connected'>
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-linkedin-in"></i>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-instagram"></i>
+                </div>
+            </div>
+            <div class='col logo-foot'>
+                <h1>Domen</h1>
+                <a href='#'>
+                    <img src='assets/images/bluegrid.png' alt='Blue Grid doo' />
+                </a>
+            </div>
+        </div>
+    </div>
 </footer>
+<script src="/assets/js/app.js"></script>
