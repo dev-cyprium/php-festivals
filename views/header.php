9e40f930a4bfffe8968149a7f4551ea3da0dbd83
Stefan Kupresak <stefan_vg@hotmail.com>
2018-06-10 19:04:21 +0200

    Add login page to the site

--- views/header.php
+++ views/header.php
@@ -3,8 +3,12 @@
         <ul>
         <?php foreach(fetchLinks($conn) as $link): ?>
             <li>
-                <a class='<?= $route['route'] == $link->name ? "active" : "" ?>' href='<?= "/" . $link->name ?>'>
-                <?= $link->labela ?>
+                <a 
+                    class='<?= $route['route'] == $link->name ? "active" : "" ?>' 
+                    href='<?= "/" . $link->name ?>'
+                    target='<?= $link->target_blank == 1 ? "_blank" : "_self" ?>'
+                >
+                        <?= $link->labela ?>
                 </a>
             </li>
         <?php endforeach ?>
---
+++ views/prijava.php
@@ -0,0 +1,13 @@
+<div class='site-form'>
+  <h1 class='site-form__title'>Prijava</h1>
+  <div class='site-form__wrap'>
+    <form class='form'>
+      <input type='text' class="form__control" placeholder='Kor. ime ili mail' />
+      <input type='password' class="form__control" placeholder='Lozinka' />
+      <button class='form__submit form__submit--primary'>
+        Prijavi se
+      </button>
+    </form>
+  </div>
+</div>
+
