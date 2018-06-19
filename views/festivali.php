aafa5d06b7483a7ec5020bbdf00db597fc7e085c
Stefan Kupresak <stefan_vg@hotmail.com>
2018-06-19 17:03:43 +0200

    add style for festivals

--- views/festivali.php
+++ views/festivali.php
@@ -3,38 +3,87 @@
     <input class='form__control' type='search' placeholder='Pretraži festivale...' />
   </form>
   <div class="festivali__wrap">
-    <h1>Prestojeći festivali</h1>
+    <h1>Svi festivali</h1>
     <div class='festivali__list'>
       <div class='festival'>
-        <div class='festival__slika'>
-        </div>
-        <h2>Naslov</h2>
-        <h3>Datum: 12/12/2016</h3>
+        <figure class='festival__slika'>
+          <img src='assets/images/beer-fest.jpg' alt='bf' />
+          <figcaption>
+            <h2>Naslov</h2>
+            <h3><i class="far fa-calendar-alt"></i>
+               12 / 12 / 2016</h3>
+            <h3><i class="fas fa-users"></i> 12 000</h3>
+          </figcaption>
+        </figure>
       </div>
+
       <div class='festival'>
-        <div class='festival__slika'>
-        </div>
-        <h2>Naslov</h2>
-        <h3>Datum: 12/12/2016</h3>
+        <figure class='festival__slika'>
+          <img src='assets/images/beer-fest.jpg' alt='bf' />
+          <figcaption>
+            <h2>Naslov</h2>
+            <h3><i class="far fa-calendar-alt"></i>
+              12 / 12 / 2016</h3>
+            <h3><i class="fas fa-users"></i> 12 000</h3>
+          </figcaption>
+        </figure>
       </div>
+
       <div class='festival'>
-        <div class='festival__slika'>
-        </div>
-        <h2>Naslov</h2>
-        <h3>Datum: 12/12/2016</h3>
+        <figure class='festival__slika'>
+          <img src='assets/images/beer-fest.jpg' alt='bf' />
+          <figcaption>
+            <h2>Naslov</h2>
+            <h3><i class="far fa-calendar-alt"></i>
+              12 / 12 / 2016</h3>
+            <h3><i class="fas fa-users"></i> 12 000</h3>
+          </figcaption>
+        </figure>
       </div>
+
       <div class='festival'>
-        <div class='festival__slika'>
-        </div>
-        <h2>Naslov</h2>
-        <h3>Datum: 12/12/2016</h3>
+        <figure class='festival__slika'>
+          <img src='assets/images/beer-fest.jpg' alt='bf' />
+          <figcaption>
+            <h2>Naslov</h2>
+            <h3><i class="far fa-calendar-alt"></i>
+              12 / 12 / 2016</h3>
+            <h3><i class="fas fa-users"></i> 12 000</h3>
+          </figcaption>
+        </figure>
       </div>
+
       <div class='festival'>
-        <div class='festival__slika'>
-        </div>
-        <h2>Naslov</h2>
-        <h3>Datum: 12/12/2016</h3>
+        <figure class='festival__slika'>
+          <img src='assets/images/beer-fest.jpg' alt='bf' />
+          <figcaption>
+            <h2>Naslov</h2>
+            <h3><i class="far fa-calendar-alt"></i>
+              12 / 12 / 2016</h3>
+            <h3><i class="fas fa-users"></i> 12 000</h3>
+          </figcaption>
+        </figure>
       </div>
+
+      <div class='festival'>
+        <figure class='festival__slika'>
+          <img src='assets/images/beer-fest.jpg' alt='bf' />
+          <figcaption>
+            <h2>Naslov</h2>
+            <h3><i class="far fa-calendar-alt"></i>
+              12 / 12 / 2016</h3>
+            <h3><i class="fas fa-users"></i> 12 000</h3>
+          </figcaption>
+        </figure>
+      </div>
+
+    </div>
+    <div class='pagination'>
+      <?php
+        for($i=1; $i<=5; $i++):?>
+          <a href='#'><?= $i ?></a>
+      <?php endfor ?>
     </div>
   </div>
+
 </div>
