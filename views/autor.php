14fad9beb539688ad846e17cd8c2842be597101d
Stefan Kupresak <stefan_vg@hotmail.com>
2018-06-22 05:56:49 +0200

    finish the autor page

---
+++ views/autor.php
@@ -0,0 +1,26 @@
+<div class='autor'>
+  <h1 class='text-center'>Stefan Kuprešak</h1>
+  <div class='autor__image'>
+    <img src='assets/images/moja.jpg'>
+    <h2>Big Data // Full Stack Web // Open Source Software</h2>
+  </div>
+  <div class='autor__bio'>
+    <p>Zdravo! S obzirom da smo svi mi "ambiciozni programeri", na ovaj ili onaj način
+       ovog puta ću preskočiti formalnosti i preći na stvar. Bavim se Big Data develpomentom
+       u programskom jeziku <strong>Elixir</strong> kao i interesovanju za moderne web frameworke
+       za full stack development poput React-a, Laravel-a i eliksirovog Feniksa.
+    </p>
+    <p>
+      Posedujem gomilu iskustva na velikom spektru polja u IT sektoru, uključujući rad na mnogim
+      projektima i to na izgradnji i skaliranju istih od nule. Trenutno sam zaposlen u firmi
+      BlueGrid, gde učestvujem na mnogim internim i spoljnim projektima. Takođe, imam veliko
+      interesovanje za experimentalne jezike poput GoLang-a i njemu sličnim pa povremeno poklanjam
+      deo svog vremena istraživanju takvih tehnologija.
+    </p>
+    <p>
+      <strong>Festivian</strong> je platforma koju sam inicijalno zamislio da sadrži mnogo više
+      zanimljivih stvari, međutim na žalost zbog vremenskih ograničenja nisam uspeo da sprovedem
+      krajnu zamisao već samo sastaviti ovaj kratak prototip nekih funkcionalnosti sa kranje platforme
+    </p>
+  </div>
+</div>
